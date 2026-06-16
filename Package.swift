a5ff3fc0feb64728ce1c06f283accd936b6bb8a2
giovannitrezzi <148251715+giovannitrezzi@users.noreply.github.com>
2026-06-16 15:13:59 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.6
+// swift-tools-version:5.9
 import PackageDescription
 
 let package = Package(
@@ -7,15 +7,22 @@ let package = Package(
         .iOS(.v14)
     ],
     products: [
-        .library(name: "ENMobileSetupSDK",  targets: ["ENMobileSetupSDKAuxiliaryTarget"]),
-        .library(name: "ENMobileSignatureSDK",  targets: ["ENMobileSignatureSDKAuxiliaryTarget"])
+        .library(
+            name: "ENMobileSetupSDK",
+            targets: ["ENMobileSetupSDKAuxiliaryTarget"]
+        ),
+        .library(
+            name: "ENMobileSignatureSDK",
+            targets: ["ENMobileSignatureSDKAuxiliaryTarget"]
+        )
     ],
     dependencies: [
         .package(url: "https://github.com/Alamofire/Alamofire.git", from: "5.8.0"),
         .package(url: "https://github.com/hmlongco/Factory", from: "2.2.0"),
-        .package(url: "https://github.com/Kitura/Swift-JWT", from: "3.6.200"),
+        .package(url: "https://github.com/Kitura/Swift-JWT", from: "4.0.0"),
         .package(url: "https://github.com/weichsel/ZIPFoundation.git", from: "0.9.17"),
-        .package(url: "https://github.com/jrendel/SwiftKeychainWrapper", from: "4.0.1")
+        .package(url: "https://github.com/jrendel/SwiftKeychainWrapper", from: "4.0.1"),
+        .package(url: "https://github.com/mattgallagher/CwlCatchException", from: "2.2.1")
     ],
     targets: [
         .target(
@@ -34,7 +41,7 @@ let package = Package(
             name: "ENMobileSetupSDKAuxiliaryTarget",
             dependencies: [
                 .target(name: "ENMobileSetupSDK"),
-                .target(name: "ENMobileCoreAuxiliaryTarget"),
+                .target(name: "ENMobileCoreAuxiliaryTarget")
             ],
             path: "ENMobileSetup"
         ),
@@ -42,30 +49,39 @@ let package = Package(
             name: "ENMobileSignatureSDKAuxiliaryTarget",
             dependencies: [
                 .target(name: "ENMobileSignatureSDK"),
-                .target(name: "ENLibPdf"),
+                .target(name: "ENMobileSetupSDKAuxiliaryTarget"),
                 .target(name: "ENMobileCoreAuxiliaryTarget"),
+                .target(name: "ENLibPdf"),
+                .target(name: "ENCrossToolbox"),
+                .product(name: "CwlCatchException", package: "CwlCatchException")
             ],
             path: "ENMobileSignature"
         ),
+
         .binaryTarget(
             name: "ENMobileCoreSDK",
-            url: "https://xcframeworks.s3.eu-south-1.amazonaws.com/ENMobileCoreSDK/0.0.1/ENMobileCoreSDK.zip",
-            checksum: "161399af74abc6adab265e8fc716091ff690a8b29cbf2df2611798c6ecd092b6"
+            url: "https://xcframeworks.s3.eu-south-1.amazonaws.com/ENMobileCoreSDK/1.0.0/ENMobileCoreSDK.zip",
+            checksum: "d1db82acc12cad6cd679dbd9a9a60c5ec71ea5f3c23f268a315deebbb8f256f3"
         ),
         .binaryTarget(
             name: "ENMobileSetupSDK",
-            url: "https://xcframeworks.s3.eu-south-1.amazonaws.com/ENMobileSetupSDK/0.0.1/ENMobileSetupSDK.zip",
-            checksum: "e1b1841d37fe6fabf0269f57b3b7b9b883dfd2df211d0672ce107d09cfa84912"
+            url: "https://xcframeworks.s3.eu-south-1.amazonaws.com/ENMobileSetupSDK/1.0.0/ENMobileSetupSDK.zip",
+            checksum: "8555591ef8c93673346a5aa8daa2deaff1be3bc5cb9ea3d6ca66150bd70d748e"
         ),
         .binaryTarget(
             name: "ENMobileSignatureSDK",
-            url: "https://xcframeworks.s3.eu-south-1.amazonaws.com/ENMobileSignatureSDK/0.0.1/ENMobileSignatureSDK.zip",
-            checksum: "d6221ada128c8510c7446c32e1f695d7a6c8747aa3b1ccda52c90c601c2a9de6"
+            url: "https://xcframeworks.s3.eu-south-1.amazonaws.com/ENMobileSignatureSDK/1.0.0/ENMobileSignatureSDK.zip",
+            checksum: "8f55204600173478047dde19869054e347231f70a3cd47431fc6964316e4876f"
         ),
         .binaryTarget(
             name: "ENLibPdf",
-            url: "https://xcframeworks.s3.eu-south-1.amazonaws.com/ENLibPdf/2.4.3/ENLibPdf.zip",
-            checksum: "9d76f45e543f597a54fb57eaea3a8994c4d85cab44d91db2e01ebd95c47a14d9"
+            url: "https://xcframeworks.s3.eu-south-1.amazonaws.com/ENLibPdf/2.4.8/ENLibPdf.zip",
+            checksum: "cc9c5bc11466f44840aad7397893b262511b2f647e6483bb2fcf3f0430c45c4c"
+        ),
+        .binaryTarget(
+            name: "ENCrossToolbox",
+            url: "https://xcframeworks.s3.eu-south-1.amazonaws.com/ENCrossToolbox/1.3.0/ENCrossToolbox.zip",
+            checksum: "77faf3cd403bd6077d2bf977ebd40a9018ab99b63dd07a2f86996a85b174baf5"
         )
     ]
 )
