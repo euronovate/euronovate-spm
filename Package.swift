c94ee44ddd30562d1a9a108084d07b64d7936c80
giovannitrezzi <148251715+giovannitrezzi@users.noreply.github.com>
2026-06-19 11:28:53 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -48,17 +48,17 @@ let package = Package(
         .binaryTarget(
             name: "ENMobileCoreSDK",
             url: "https://xcframeworks.s3.eu-south-1.amazonaws.com/ENMobileCoreSDK/0.9.0/ENMobileCoreSDK.zip",
-            checksum: "3e2ccfb11f3fade86f3c201034f3731540c92d82c4799e4ddae00d5834ea20bb"
+            checksum: "30923bb107c3209e84d6565084d2f7d5e71605c229193d58c095f634c6e8a302"
         ),
         .binaryTarget(
             name: "ENMobileSetupSDK",
             url: "https://xcframeworks.s3.eu-south-1.amazonaws.com/ENMobileSetupSDK/0.9.0/ENMobileSetupSDK.zip",
-            checksum: "c3fc1eea7294c8791ad8b4cf3587b9f7b754f0c735944072519ca605aa4b4ebc"
+            checksum: "60b804140819bcc71564204b749dd67c88783ecbf7a5fdcec05930bb4a52d766"
         ),
         .binaryTarget(
             name: "ENMobileSignatureSDK",
             url: "https://xcframeworks.s3.eu-south-1.amazonaws.com/ENMobileSignatureSDK/0.9.0/ENMobileSignatureSDK.zip",
-            checksum: "ed9988e913eb81cb7f5c697f68031900e4328eb6073f57e1331a16dd28ea5394"
+            checksum: "f58d1d8d5c7908ad171d07fabfe96d10c2723e24fc3693c3d17dd1bb95df004b"
         ),
         .binaryTarget(
             name: "ENLibPdf",
